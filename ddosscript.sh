04ddf45afa5cf71ba2f603fb80ad3b4c050d49d6
Andrii-Asiox <60571856+Andrii-Asiox@users.noreply.github.com>
2022-08-07 23:18:42 +0300

    0.9.17

--- ddosscript.sh
+++ ddosscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dbLink="https://github.com/arriven/db1000n/releases/download/v0.9.16/db1000n_linux_arm64.tar.gz"
+dbLink="https://github.com/arriven/db1000n/releases/download/v0.9.17/db1000n_linux_arm64.tar.gz"
 dbTarFile="db1000n_arm64_archive.tar.gz"
 
 rm db1000n*
 wget -O $dbTarFile "$dbLink"
 tar -xvzf $dbTarFile
 
-echo "updated 0.9.16"
+echo "updated 0.9.17"
